5d5962e64ec0d8e98d7bf36fa6ba8dc5b6acb449
Brett Bowman <bbowman@pacificbiosciences.com>
2013-05-30 15:39:15 -0700

    Reworking of post-HGAP pipeline up to phasing

---
+++ src/pbhla/reference/ReferenceDict.py
@@ -0,0 +1,108 @@
+import csv, logging
+
+from pbhla.utils import BlasrM1
+from pbhla.io.SamIO import SamReader
+from pbcore.io.FastaIO import FastaReader
+
+class ReferenceDict( object ): 
+
+    def __init__(self, input_file, reference=None):
+        self._dict = {}
+        self.input_file = input_file
+        self.file_type = self.input_file.split('.')[-1]
+        self.reference = reference
+        self.initialize_logger()
+        self.validate_input()
+        self.run()
+
+    def initialize_logger(self):
+        self.log = logging.getLogger(__name__)
+        self.log.info("Initializing LocusDict with the following:")
+        self.log.info("\tInput File: {0}".format(self.input_file))
+        self.log.info("\tReference: {0}".format(self.reference))
+
+    def validate_input(self):
+        if self.file_type in ['fofn', 'txt', 'm1', 'sam']:
+            pass
+        else:
+            msg = 'Unrecognized Alignment file-type! "{0}"'.format(self.input_file)
+            self.log.info( msg )
+            raise ValueError( msg )
+
+    def run(self):
+        if self.file_type == 'fofn':
+            self.parse_reference_fofn()
+        if self.file_type == 'txt':
+            self.parse_locus_key()
+        if self.file_type == 'm1':
+            self.parse_blasr_file()
+        if self.file_type == 'sam':
+            self.parse_sam_file()
+
+    def parse_reference_fofn(self):
+        msg = 'Reading Locus References from "{0}"'.format(self.input_file)
+        self.log.info( msg )
+        with open(self.input_file, 'r') as handle:
+            for line in handle:
+                fasta_file, locus = line.strip().split()
+                msg = 'Reading "{0}" sequences from "{1}"'.format(locus, fasta_file)
+                self.log.info( msg )
+                for record in FastaReader( fasta_file ):
+                    name = record.name.split()[0]
+                    self[name] = locus
+        self.log.info('Finished reading Locus References')
+
+    def parse_locus_key(self):
+        msg = 'Reading existing Locus Key from "{0}"'.format(self.input_file)
+        self.log.info( msg )
+        with open(self.input_file, 'r') as handle:
+            for line in handle:
+                seq_name, locus = line.strip().split()
+                self[seq_name] = locus
+        self.log.info('Finished reading Locus Key')
+
+    def parse_blasr_file(self):
+        msg = 'Parsing Blasr results from "{0}"'.format(self.input_file)
+        self.log.info( msg )
+        with open(self.input_file, 'r') as handle:
+            for hit in map(BlasrM1._make, csv.reader(handle, delimiter=' ')):
+                key = hit.qname.split('/')[0]
+                if self.reference:
+                    self[key] = self.reference[hit.tname]
+                else:
+                    self[key] = hit.tname
+        self.log.info('Finished reading Blasr results')
+
+    def parse_sam_file(self):
+        msg = 'Parsing SAM alignments from "{0}"'.format(self.input_file)
+        self.log.info( msg )
+        for record in SamReader(self.input_file):
+            key = record.qname.split('/')[0]
+            if self.reference:
+                self[key] = self.reference[record.rname]
+            else:
+                self[key] = record.rname
+        self.log.info('Finished reading SAM file results')
+
+    def write(self, output_file):
+        msg = 'Writing new Locus Key to "{0}"'.format(output_file)
+        self.log.info( msg )
+        with open(output_file, 'w') as handle:
+            for seq_name, locus in self._dict.iteritems():
+                print >> handle, '{0} {1}'.format(seq_name, locus)
+
+    def __setitem__(self, key, value):
+        if key in self._dict:
+            msg = 'Duplicate sequence ids found! "{0}"'.format( key )
+            self.log.info( msg )
+            raise KeyError( msg )
+        self._dict[key] = value
+
+    def __getitem__(self, key):
+        return self._dict[key]
+
+    def __delitem__(self, key):
+        del self._dict[key]
+
+    def __iter__(self):
+        return iter(self._dict)
---
+++ src/pbhla/reference/SequenceSeparator.py
@@ -0,0 +1,84 @@
+import os, logging
+
+from pbcore.io.FastaIO import FastaReader, FastaWriter
+
+class SequenceSeparator( object ): 
+
+    def __init__(self, fasta_file, reference_dict, selected=None):
+        self.fasta_file = fasta_file
+        self.reference_dict = reference_dict
+        self.selected = selected
+        self.sequences = {}
+        self.initialize_logger()
+        self.validate_input_file()
+        self.run()
+
+    def initialize_logger(self):
+        self.log = logging.getLogger(__name__)
+        self.log.info("Initializing LocusSeparator with the following:")
+        self.log.info("\tFasta File: {0}".format(self.fasta_file))
+        self.log.info("\tSelected Values: {0}".format(self.selected))
+
+    def validate_input_file( self ):
+        if self.fasta_file.endswith('.fa') or self.fasta_file.endswith('.fasta'):
+            pass
+        else:
+            msg = 'Unrecognized Fasta file-type! "{0}"'.format(self.fasta_file)
+            self.log.info( msg )
+            raise ValueError( msg )
+
+    def run(self):
+        if self.selected:
+            self.separate_selected()
+        else:
+            self.separate_by_reference()
+
+    def separate_selected(self):
+        self.log.info('Separating selected references')
+        for record in FastaReader( self.fasta_file ):
+            name = record.name.split()[0]
+            ref = self.get_reference( name )
+            if ref in self.selected:
+                self.add_record( record, 'selected' )
+            else:
+                self.add_record( record, 'not_selected' )
+        self.log.info('Finished separating sequences')
+
+    def separate_by_reference(self):
+        self.log.info('Separating sequences by reference')
+        for record in FastaReader( self.fasta_file ):
+            name = record.name.split()[0]
+            ref = self.get_reference( name )
+            self.add_record( record, ref )
+        self.log.info('Finished separating sequences')
+
+    def get_reference(self, name):
+        try:
+            reference = self.reference_dict[name]
+        except:
+            reference = 'Unmapped'
+        return reference
+
+    def add_record(self, record, locus):
+        try:
+            self.sequences[locus].append( record )
+        except:
+            self.sequences[locus] = [ record ]
+
+    def write(self, value, output_file):
+        self.log.info('Writing "{0}" sequences to "{1}"'.format(value, output_file))
+        with FastaWriter( output_file ) as handle:
+            for record in self.sequences[value]:
+                handle.writeRecord( record )
+        self.log.info('Finished writing sequences from "{0}"'.format(value))
+        return output_file
+
+    def write_all(self, prefix='Locus'):
+        self.log.info('Writing fasta sequences from all loci to file')
+        output_files = []
+        for value in self.sequences:
+            output_file = '{0}_{1}.fasta'.format(prefix, value)
+            output_file = self.write(value, output_file)
+            output_files.append( output_file )
+        self.log.info('Finished writing sequences from all loci')
+        return output_files
