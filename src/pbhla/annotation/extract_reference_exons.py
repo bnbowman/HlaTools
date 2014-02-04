4e8bfb43285dbc26f701f8e3e4912f3d2e0006af
Brett Bowman <bnbowman@gmail.com>
2014-02-04 15:22:11 -0800

    Clean up for new directory org

--- src/pbhla/annotation/extract_reference_exons.py
+++ src/pbhla/annotation/extract_reference_exons.py
@@ -1,31 +1,27 @@
 #! /usr/bin/env python
 
-import sys, csv
+import sys, csv, os
 
 from collections import namedtuple
-from pbcore.io.FastaIO import FastaReader, FastaRecord, FastaWriter
+from pbcore.io.FastaIO import FastaRecord, FastaWriter, FastaReader
 
 AfaInfo = namedtuple('AfaInfo', 'pos0 pos1 region codon')
 
 def extract_exons( afa_file, info_file):
-    records = list(_parse_aligned_fasta( afa_file ))
+    locus = afa_file.split('_')[0]
+    output_fofn = '%s_exons.fofn' % locus
+    records = list( FastaReader( afa_file ))
     regions = list(_parse_info_file( info_file ))
-    for exon in _select_exons( regions ):
-        output_file = _get_output_file( info_file, exon )
-        with FastaWriter( output_file ) as output:
-            for record in _extract_fasta_region( records, exon ):
-                if len(set(record.sequence)) == 1:
-                    continue
-                output.writeRecord( record )
-    
-def _parse_aligned_fasta( afa_file ):
-    for i, record in enumerate(FastaReader( afa_file )):
-        if i == 0:
-            length = len(record.sequence)
-        if len(record.sequence) == length:
-            yield record
-        else:
-            raise ValueError( 'Uneven sequence length!')
+
+    with open( output_fofn, 'w' ) as fofn_handle:
+        for exon in _select_exons( regions ):
+            output_file = _get_output_file( info_file, exon )
+            with FastaWriter( output_file ) as output:
+                for record in _extract_fasta_region( records, exon ):
+                    if len(set(record.sequence)) == 1:
+                        continue
+                    output.writeRecord( record )
+            fofn_handle.write( os.path.abspath( output_file ) + '\n' )
 
 def _parse_info_file( info_file ):
     region = None
---
+++ src/pbhla/references/fasta.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+
+__author__ = 'Brett Bowman'
+__email__ = 'bbowman@pacificbiosciences.com'
+
