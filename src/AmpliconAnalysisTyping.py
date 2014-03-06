6c8df576b6a99ecfc5c26b41c44a6873d363419c
Brett Bowman <bbowman@pacificbiosciences.com>
2014-03-06 10:01:49 -0800

    Updating AAT to use ArgParse like a reasonable tool

--- src/AmpliconAnalysisTyping.py
+++ src/AmpliconAnalysisTyping.py
@@ -3,12 +3,25 @@
 from pbhla.typing.sequences import type_sequences
 
 if __name__ == '__main__':
-    import sys
+    import argparse
+    parser = argparse.ArgumentParser()
 
-    input = sys.argv[1]
-    grouping = sys.argv[2] if len(sys.argv) > 2 else None
-    exon_fofn = sys.argv[3] if len(sys.argv) > 3 else None
-    genomic_reference = sys.argv[4] if len(sys.argv) > 4 else None
-    cDNA_reference = sys.argv[5] if len(sys.argv) > 5 else None
+    add = parser.add_argument
+    add('amplicon_analysis', metavar='INPUT',
+        help="Fasta/Fastq/Folder of Amplicon Analysis output")
+    add('-g', '--grouping', metavar='METHOD', default='all',
+        help="BasH5 or FOFN of sequence data")
+    add('-e', '--exon_reference', metavar='REFERENCE', default=None,
+        help='Dictionary file of Locus-specific exon references')
+    add('-n', '--nucleotide_reference', metavar='FASTA', default=None,
+        help='File of FASTA sequences from nucleotide references')
+    add('-c', '--cDNA_reference', metavar='FASTA', default=None,
+        help='File of FASTA sequences from cDNA references')
+    add('--debug', action='store_true',
+        help="Flag to enable Debug mode")
+    args = parser.parse_args()
 
-    type_sequences( input, grouping, exon_fofn, genomic_reference, cDNA_reference )
+    type_sequences( args.amplicon_analysis, args.grouping,
+                                            args.exon_reference,
+                                            args.nucleotide_reference,
+                                            args.cDNA_reference )
